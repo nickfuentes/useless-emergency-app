a3338e78541164a7c1acad0ad986f26004afa562
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-08-14 13:46:24 -0500

    added register

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,20 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
+router.get("/", function(req, res, next) {
   res.render("index", {
     title: "Useless Emergency App"
   });
 });
 
-router.get("/login", function (req, res, next) {
+router.get("/login", function(req, res, next) {
   res.render("login", {
+    title: "Useless Emergency App"
+  });
+});
+
+router.get("/register", function(req, res, next) {
+  res.render("register", {
     title: "Useless Emergency App"
   });
 });
