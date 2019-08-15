9b7f2e7cfd99f7d174975a427f164f419f0970b9
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-08-15 18:20:29 -0500

    adding cloud functions

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
+const functions = require("firebase-functions");
 const express = require("express");
 const app = express();
 const session = require("express-session");
 const http = require("http");
-const path = require('path')
+const path = require("path");
 
-
-const mustacheExpress = require("mustache-express")
-const VIEWS_PATH = path.join(__dirname, '/views')
+const mustacheExpress = require("mustache-express");
+const VIEWS_PATH = path.join(__dirname, "/views");
 
 const indexRouter = require("./routes/index");
 
@@ -14,37 +14,37 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-
-app.engine("mustache", mustacheExpress(VIEWS_PATH + '/partials', '.mustache'))
-app.set("views", VIEWS_PATH)
+app.engine("mustache", mustacheExpress(VIEWS_PATH + "/partials", ".mustache"));
+app.set("views", VIEWS_PATH);
 app.set("view engine", "mustache");
 
 app.use(
-    session({
-        secret: "keyboard cat",
-        resave: false,
-        saveUninitialized: true
-    })
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true
+  })
 );
 
 app.use("/", indexRouter);
 
 function normalizePort(val) {
-    var port = parseInt(val, 10);
+  var port = parseInt(val, 10);
 
-    if (isNaN(port)) {
-        // named pipe
-        return val;
-    }
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
 
-    if (port >= 0) {
-        // port number
-        return port;
-    }
+  if (port >= 0) {
+    // port number
+    return port;
+  }
 
-    return false;
+  return false;
 }
 
 const port = normalizePort(process.env.PORT || "3000");
 app.set("port", port);
 app.listen(port);
+exports.app = functions.https.onRequest(app);
